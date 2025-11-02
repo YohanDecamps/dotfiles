d285b79e6235a9e62a3f8dd2a3f78edc3c4825a3
YohanDecamps <yohan.decamps@epitech.eu>
2025-11-02 10:32:56 -0500

    feat: typescript tools

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -4,9 +4,8 @@ return {
         config = function()
             local defaults = require("nvchad.configs.lspconfig")
             defaults.defaults()
-
+            
             -- Clangd configuration
-
             local clangd_config =
                 vim.tbl_deep_extend(
                 "force",
@@ -14,17 +13,26 @@ return {
                 {on_attach = defaults.on_attach, capabilities = defaults.capabilities}
             )
             
+            -- TypeScript configuration
+            local ts_config = {
+                on_attach = defaults.on_attach,
+                capabilities = defaults.capabilities,
+                filetypes = {"typescript", "typescriptreact", "javascript", "javascriptreact"},
+                cmd = {"typescript-language-server", "--stdio"}
+            }
+            
             -- Setup LSPs depending on Neovim version
-
             if vim.lsp and vim.lsp.start then
                 vim.lsp.config("clangd", clangd_config)
                 vim.lsp.enable("clangd")
                 
+                vim.lsp.config("ts_ls", ts_config)
+                vim.lsp.enable("ts_ls")
             else
                 local lspconfig = require("lspconfig")
                 lspconfig.clangd.setup(clangd_config)
+                lspconfig.ts_ls.setup(ts_config)
             end
         end
     }
 }
-
